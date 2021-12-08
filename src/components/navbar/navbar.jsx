bf2830cf3f37d49946c9e78b5657d536dafda759
Ben Bissett <benjaminbissett@gmail.com>
2021-12-08 10:53:06 -0700

    Added user's username to the navbar as a link to their profile. Added a working logout button.

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
 const NavBar = (props) => {
-  const user = props.user;
+
+  const [user, userLogged] = useState();
+  const [jwt, setJWT] = useState();
+  const [userInfo, setUserInfo] = useState();
+  
+  useEffect(() => {
+      getJWT();
+  }, [])
+  useEffect(() => {
+      getUser();
+  }, [jwt])
+  useEffect(() => {
+      console.log(user);
+      if (user){
+        getUserInfo();
+      }
+  }, [user])
+  
+  function getJWT() {
+      const jwt = localStorage.getItem('token');
+      setJWT(jwt);        
+  }
+  function getUser() {
+      try{
+          const user = jwtDecode(jwt);
+          userLogged(user);
+      } catch {}
+  }
+
+  const getUserInfo = async () => {
+    let pk = user.user_id;
+    const userInfo = await axios.get(`http://127.0.0.1:8000/api/auth/user/${pk}`, { headers: { Authorization: `Bearer ${jwt}` } });
+    if(userInfo) {
+        setUserInfo(userInfo.data);
+    } else {console.log('no user found')}
+  }
+  
+  function refreshPage() {
+    window.location.reload();
+  }
+
+  function deleteJWT() {
+    localStorage.removeItem('token');
+    refreshPage();
+  }
+
+
     return ( 
 <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
   <div className="container-fluid">
@@ -30,23 +78,26 @@ const NavBar = (props) => {
             <a className="dropdown-item" href="#">Separated link</a>
           </div>
         </li> */}
+        {!userInfo ?
+        <ul className="navbar-nav me-auto">
         <li className="nav-item">
         <a className="nav-link" href="/login">Login
             <span className="visually-hidden">(current)</span>
           </a>
         </li>
-
-        {user ? 
-        <li className="nav-item">
-          <a className="nav-link" href="/user">{user.username}</a>
-        </li>
-        : 
         <li className="nav-item">
         <a className="nav-link" href="/register">Register
             <span className="visually-hidden">(current)</span>
           </a>
-        </li>}
-      </ul>
+        </li></ul>
+        :
+        <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+        <a className="nav-link" href="/user">{userInfo.username}'s Profile</a>
+        </li>
+        <button type="button" class="btn btn-primary btn-rg" onClick={deleteJWT}>Logout</button>
+        </ul>}
+        </ul>
       {/* <form className="d-flex">
         <input className="form-control me-sm-2" type="text" placeholder="Search" />
         <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
--- src/components/splash/splash.jsx
+++ src/components/splash/splash.jsx
@@ -1,35 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import NavBar from '../navbar/navbar';
 import MapContainer from '../MapContainer/MapContainer';
-import jwtDecode from 'jwt-decode';
 
 
 const Splash = (props) => {
-
-    const [user, userLogged] = useState();
-    const [jwt, setJWT] = useState();
-
-    function getJWT() {
-        const jwt = localStorage.getItem('token');
-        setJWT(jwt);        
-    }
-    function getUser() {
-        try{
-            const user = jwtDecode(jwt);
-            userLogged(user);
-        } catch {}
-    }
-
-    useEffect(() => {
-        getJWT();
-    }, [])
-    useEffect(() => {
-        console.log(jwt);
-        getUser();
-    }, [jwt])
-    useEffect(() => {
-        console.log(user);
-    }, [user])
 
     return ( 
         <div>
